faa1d1617191f40d058bd796e008cbf37fa07d74
Anderson Keller <63135895+AndersonKeller@users.noreply.github.com>
2025-03-17 21:25:22 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,43 @@
+// // number - qualquer número 
+// // string - texto
+// // boolean - true ou false (verdadeiro ou falso)
+
+// //TIPOS DE VARIÁVEIS
+// // let - variável que pode ter seu valor modificado
+// // const - variável que não pode ter seu valor modificado - (imutável)
+// let nome = "Anderson"
+// const sobrenome = "Keller"
+// console.log("olá, meu nome é " + nome + " " + sobrenome)
+
+
+// console.log(`olá, meu nome é ${nome} ${sobrenome}`)
+
+// //
+// console.log(nome+sobrenome)
+// //operações matematicas
+// // + - / *
+
+// let numeroa = 10
+// let numerob = 20
+// let soma = numeroa * numerob
+// console.log(`A soma de ${numeroa} + ${numerob} é: ${soma}`)
+
+
+// Exercício 1
+
+// Declare uma variável chamada “nome”; Essa variável vai receber um valor do tipo string: 
+// o seu nome; Em seguida, imprima o seguinte no 
+// console: “Olá, eu sou a/o seu-nome-aqui”, utilizando a variável declarada anteriormente.
+
+let nome = "Anderson"
+console.log(`Olá, eu sou o ${nome}`)
+
+// Exercício 2 
+// Declare duas variáveis, uma chamada “nome” e outra chamada “sobrenome”; 
+// As duas vão receber um valor do tipo string. Na primeira atribua o seu nome, 
+// e na segunda atribua o seu sobrenome; Para finalizar, 
+// imprima o seguinte no console: “Olá, eu me chamo seu-nome-aqui seu-sobrenome-aqui”, 
+// utilizando as variáveis declaradas anteriormente.
+let nome2 = "Anderson"
+let sobrenome = "Keller"
+console.log(`Olá, eu me chamo ${nome2} ${sobrenome}`)
