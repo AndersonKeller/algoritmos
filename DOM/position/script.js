d379afac0edbbc4d61a07591a70b34c4fc34ff51
Anderson Keller <63135895+AndersonKeller@users.noreply.github.com>
2025-04-14 20:12:49 -0300

    Update script.js

--- DOM/position/script.js
+++ DOM/position/script.js
@@ -1,4 +1,6 @@
 
+import { data } from "./data.js"
+let carrinho = []
 
 function openCarrinho(){
     const button = document.querySelector(".carrrinhobtn")
@@ -8,20 +10,35 @@ function openCarrinho(){
         if(ulcart){
             ulcart.remove()
         }else{
-            const header = document.querySelector("header")
-        header.insertAdjacentHTML("beforeend",`
-             <ul class="cartul">
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-        </ul>
+            carrinho.forEach((cart)=>{
+                const header = document.querySelector("header")
+            header.insertAdjacentHTML("beforeend",`
+                <ul class="cartul">
+                <li>${cart.name}</li>
+                </ul>
             `)
+            })
         }
         
     })
 }
 openCarrinho()
+
+function montarLista(){
+    const ulProdutos = document.querySelector(".produtos")
+    data.items.forEach((item)=>{
+    ulProdutos.insertAdjacentHTML("beforeend",`
+        <li>
+            <p>${item.product.name}</p>
+            <button id="btnadd${item.product.id}" >Adicionar ao carrinho</button>
+        </li>
+        
+        `)
+        const btnadd = document.getElementById(`btnadd${item.product.id}`)
+        btnadd.addEventListener("click",()=>{
+            console.log("add ao carrinho",item.product)
+            carrinho.push(item.product)
+        })
+    })
+}
+montarLista()
